becdf28b24e0e6c016ed292b2e4e02db60b7bb30
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2023-08-18 21:15:57 +0530

    Alarm Clock Extension for Visual Studio Code

---
+++ README.md
@@ -0,0 +1,36 @@
+# Alarm Clock Extension for Visual Studio Code
+
+The Alarm Clock Extension for Visual Studio Code allows you to set alarms and receive notifications right within your favorite code editor. Never miss an important deadline or meeting again with this simple and handy tool!
+
+## Features
+
+- Set alarms easily using the "Set Alarm" command.
+- Input alarm times in a user-friendly format (HH:MM AM/PM).
+- Receive a notification when the alarm time is reached.
+- Configurable to your preferences for a seamless experience.
+
+## Installation
+
+1. Launch Visual Studio Code.
+2. Go to the Extensions view by clicking on the square icon in the sidebar.
+3. Search for "Alarm Clock" and click Install.
+
+## Usage
+
+1. Open the Command Palette (Ctrl+Shift+P) and search for "Set Alarm."
+2. Enter the desired alarm time in the format "HH:MM AM/PM" (e.g., "08:30 AM" or "10:00 PM").
+3. Receive a notification when the specified alarm time arrives.
+
+Stay productive and punctual with the Alarm Clock Extension for Visual Studio Code!
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+**Note:** This extension is not intended to replace dedicated alarm clock software or critical time-sensitive applications. It is designed to provide a convenient way to set reminders within the Visual Studio Code environment.
---
+++ extension.js
@@ -0,0 +1,85 @@
+const vscode = require('vscode');
+
+let alarmInterval;
+let alarmTime;
+let alarmIntervalId;
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Register the "Set Alarm" command
+  let disposableSetAlarm = vscode.commands.registerCommand('alarmclock.setAlarm', () => {
+    vscode.window.showInputBox({ prompt: 'Enter alarm time (HH:MM AM/PM)' }).then((time) => {
+      if (time) {
+        setAlarm(time);
+      }
+    });
+  });
+
+  context.subscriptions.push(disposableSetAlarm);
+}
+
+function setAlarm(timeString) {
+  if (alarmInterval) {
+    clearInterval(alarmIntervalId);
+  }
+
+  alarmTime = parseTime(timeString);
+
+  if (alarmTime) {
+    alarmInterval = true;
+    alarmIntervalId = setInterval(checkAlarm, 1000);
+    vscode.window.showInformationMessage(`Alarm set for ${formatTime(alarmTime)}.`);
+  } else {
+    vscode.window.showErrorMessage('Invalid time format. Please use HH:MM AM/PM.');
+  }
+}
+
+function parseTime(timeString) {
+  const timePattern = /(\d{1,2}):(\d{2})\s*(AM|PM|am|pm)?/;
+  const matches = timeString.match(timePattern);
+
+  if (matches) {
+    let hours = parseInt(matches[1]);
+    let minutes = parseInt(matches[2]);
+    const meridiem = matches[3] ? matches[3].toLowerCase() : 'am';
+
+    if (meridiem === 'pm' && hours !== 12) {
+      hours += 12;
+    } else if (meridiem === 'am' && hours === 12) {
+      hours = 0;
+    }
+
+    return new Date(0, 0, 0, hours, minutes);
+  }
+
+  return null;
+}
+
+function checkAlarm() {
+  const now = new Date();
+  if (alarmTime && now.getHours() === alarmTime.getHours() && now.getMinutes() === alarmTime.getMinutes()) {
+    clearInterval(alarmIntervalId);
+    vscode.window.showInformationMessage('Time to wake up! Alarm is ringing.');
+  }
+}
+
+function formatTime(time) {
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const meridiem = hours >= 12 ? 'PM' : 'AM';
+  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+  return `${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${meridiem}`;
+}
+
+function deactivate() {
+  if (alarmInterval) {
+    clearInterval(alarmIntervalId);
+  }
+}
+
+module.exports = {
+  activate,
+  deactivate
+};
